9cfbc34f4b9da769c822ad7dab8bb02d393ff3b2
mXz <Awojidemartins@gmail.com>
2024-11-03 00:43:25 +0200

    UPDATE: full val_num_samples for eval

--- train_main.sh
+++ train_main.sh
@@ -5,13 +5,13 @@ python -m open_clip_train.main \
     --beta2 0.95 \
     --save-frequency 1 \
     --zeroshot-frequency 1 \
-    --save-most-recent True\
+    --save-most-recent \
     --train-data './data/mscoco_full/{00000..00049}.tar' \
     --val-data './data/mscoco_full/{00050..00059}.tar' \
     --dataset-type webdataset \
     --dataset-resampled \
     --train-num-samples 50000 \
-    --val-num-samples 10000 \
+    --val-num-samples 100000 \
     --imagenet-val /data/imagenet/validation/ \
     --warmup 5000 \
     --batch-size 1024 \
