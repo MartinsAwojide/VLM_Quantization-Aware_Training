d7edc5b768b86f9635dcde13d13362836e731adb
mXz <Awojidemartins@gmail.com>
2024-11-03 00:07:55 +0200

    UPDATE: training script with imagenet zero-shot eval and data resampling

--- train_main.sh
+++ train_main.sh
@@ -3,13 +3,16 @@
 python -m open_clip_train.main \
     --beta1 0.9 \
     --beta2 0.95 \
-    --save-frequency 10 \
+    --save-frequency 1 \
+    --zeroshot-frequency 1 \
     --save-most-recent True\
     --train-data './data/mscoco_full/{00000..00049}.tar' \
     --val-data './data/mscoco_full/{00050..00059}.tar' \
     --dataset-type webdataset \
     --dataset-resampled \
-    --train-num-samples 500000 \
+    --train-num-samples 50000 \
+    --val-num-samples 10000 \
+    --imagenet-val /data/imagenet/validation/ \
     --warmup 5000 \
     --batch-size 1024 \
     --lr=2e-3 \
