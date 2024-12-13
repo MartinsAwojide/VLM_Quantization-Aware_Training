e4b2c5a871f40e534c411cc7091fa1b516628715
gigwegbe <gigwegbe@gmail.com>
2024-12-13 07:11:46 +0200

    Added adafactor  run script

---
+++ train_adafactor.sh
@@ -0,0 +1,31 @@
+#! /bin/bash
+
+python -m open_clip_train.main \
+    --beta1 0.9 \
+    --beta2 0.95 \
+    --save-frequency 1 \
+    --zeroshot-frequency 1 \
+    --save-most-recent \
+    --train-data './data/mscoco_full/{00000..00059}.tar' \
+    --imagenet-val './data/imagenet/validation/' \
+    --dataset-type webdataset \
+    --dataset-resampled \
+    --train-num-samples 60000 \
+    --warmup 5000 \
+    --batch-size 256 \
+    --lr=2e-3 \
+    --precision amp \
+    --wd=0.2 \
+    --epochs=200 \
+    --workers=8 \
+    --seed 0 \
+    --model RN50\
+    --opt timm/adafactorbv \
+    --report-to wandb \
+    --wandb-project open_clip_int8_setup \
+    --resume "latest" \
+    --ddp-static-graph \
+    --use-bnb-linear SwitchBackLinearGlobalMemEfficient \
+    --delete-previous-checkpoint \
+    --grad-checkpointing \
+    --wandb-notes "int8 with SwitchBackM and gradient checkpointing for memory mgt - adafactorbv"
